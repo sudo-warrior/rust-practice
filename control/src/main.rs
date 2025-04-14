1d8ae8f545d96195bd556cfe7309ce9715f9016c
The coding cove <lawrencewafula408@gmail.com>
2025-04-14 21:14:27 +0300

    added sample implementation for the if control statement

--- control/src/main.rs
+++ control/src/main.rs
@@ -12,15 +12,35 @@ fn main() {
     
     // println!("The weather  is {}", weather);
     //
-    let mut attempts = 0;
+    //let mut attempts = 0;
 
-    loop {
-        println!("connecting");
-        attempts += 1;
+    //loop {
+     //   println!("connecting");
+       // attempts += 1;
 
-        if attempts >= 3 {
-            println!("Failed after 3 attempts");
-            break;
+       // i/f attempts >= 3 {
+         //   println!("Failed after 3 attempts");
+           // break;
+       // }
+    //}
+    let mut count = 0;
+    
+    'counting_up: loop {
+        println!("count = {count}");
+        let mut remaining = 10;
+
+        loop {
+            println!("remaining: {remaining}");
+            if remaining == 9 {
+                break;
+            }
+            if count == 2 {
+                break 'counting_up;
+            }
+            remaining -= 1;
         }
+        count += 1;
     }
+    println!("End count: {count}");
+
 }
