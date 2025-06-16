8002955248d41126d8016f00e6fb93995d01a4ff
sudo-warrior <lawrencewafula408@gmail.com>
2025-06-16 04:06:08 +0000

    added changes

---
+++ loops/src/main.rs
@@ -0,0 +1,23 @@
+fn main() {
+    //type annotation
+    let mut count: i64 = 0;
+    
+    // the loop 
+    'counting_loop: loop {
+        println!("Count = {count}");
+        let mut remaining = 10;
+        loop {
+            println!("Remaining = {remaining}");
+            if remaining == 9 {
+                break;
+            }
+
+            if count == 2 {
+                break 'counting_loop;
+            }
+            remaining -= 1;
+        }
+        count += 1;
+    };
+    println!("The end count: {count}");
+}
