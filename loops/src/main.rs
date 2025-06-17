e8a3d65fa2fcf0381449be99a484f1691671f68c
sudo-warrior <lawrencewafula408@gmail.com>
2025-06-17 03:23:17 +0000

    added a new file

--- loops/src/main.rs
+++ loops/src/main.rs
@@ -1,4 +1,9 @@
 fn main() {
     // rust ownership
+    let mut s = String::from("hello");
     
+    // push_str() appends a literal to a string
+    s.push_str(", world!");
+
+    println!("{s}");
 }
