6d6e8f2e9854a243736cd17e0ef936c7f73bf1f9
sudo-warrior <lawrencewafula408@gmail.com>
2025-06-19 12:52:45 +0000

    added a new changes

--- loops/src/main.rs
+++ loops/src/main.rs
@@ -1,7 +1,24 @@
 fn main() {
-    println!("Hello, world!");
+    // println!("Hello, world!");
+    // structs
+    let mut user1 = User{
+        active: true,
+        username: String::from("someoneusername123"),
+        email: String::from("someoneusername123@gmail.com"),
+        sign_in_count: 1,
+    };
+
+    let mut user2 = User{
+        active: user1.active,
+        username: user1.username,
+        email: String::from("another@example.com"),
+        sign_in_count: user1.sign_in_count,
+    };
+
+    user1.email = String::from("anotheremail@example.com");
+    println!("{}", user1.email);
 }
 
-fn hello(s: &str) -> &str{
-    println!("Hello, world!");
-}
+//fn hello(s: &str) -> &str{
+  //  println!("Hello, world!");
+//}
